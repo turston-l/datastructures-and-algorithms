d97ee8b130b6d691a9d3e8b24e909953f51224be
turston-l <louwturston@gmail.com>
2022-05-04 15:23:11 +0200

    Adding two given variables

---
+++ sum_of_two.js
@@ -0,0 +1,9 @@
+// Function that adds two variables together
+
+function sumOfTwo(a, b){
+    // Returning the sum of two variables and storing it in sumOfTwo
+    return a + b;
+}
+
+// Example input
+sumOfTwo(2, 3);
